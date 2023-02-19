f00af049431afe6cfcd332a273439883d01826fe
Fraser Morrison <frazemor@hotmail.com>
2023-02-19 15:38:50 -0500

    Added Threlte

--- svelte.config.js
+++ svelte.config.js
@@ -1,7 +1,9 @@
 import { vitePreprocess } from '@sveltejs/vite-plugin-svelte'
+import seqPreprocessor from 'svelte-sequential-preprocessor'
+import { preprocessThrelte } from '@threlte/preprocess'
 
 export default {
   // Consult https://svelte.dev/docs#compile-time-svelte-preprocess
   // for more information about preprocessors
-  preprocess: vitePreprocess(),
+  preprocess: seqPreprocessor(vitePreprocess(), preprocessThrelte())
 }
